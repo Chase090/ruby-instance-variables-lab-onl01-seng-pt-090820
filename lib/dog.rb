dc84133e94836f702b023fe6ac89ec989e3471ab
Chase090 <carloyam21@icloud.com>
2020-11-10 19:19:08 -0800

    commit tree 7bf0476aad7f4a62cf13164f20332aa683974996

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,67 @@
+require 'pry'
+
 class Dog
 
-  def name=(dogs_name)
-    @this_dogs_name = dogs_name
-  end
+  
+    def name=(dogs_name)
+      @this_dogs_name = dogs_name
+    end
 
-  def name
-    @this_dogs_name
-  end
+    def name
+      @this_dogs_name
+    end
+
+  # binding.pry
+
 end
 
-lassie = Dog.new
-lassie.name = "Lassie"
 
-puts lassie.name
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Dog
+
+#   def name=(dogs_name)
+#     @this_dogs_name = dogs_name
+#   end
+
+#   def name
+#     @this_dogs_name
+#   end
+# end
+
+# lassie = Dog.new
+# lassie.name = "Lassie"
+
+# puts lassie.name
